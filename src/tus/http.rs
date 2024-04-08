b47c2094fa0f5835553383f4ed2b6740a0c3d89f
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2024-04-08 16:40:51 -0400

    quick refactor

--- src/tus/http.rs
+++ src/tus/http.rs
@@ -1,10 +1,7 @@
-use crate::{
-    error::TusError,
-    tus::{self, headers::Headers},
-};
+use crate::{error::TusError, tus::headers::Headers};
 use reqwest::Method;
 use serde::{Deserialize, Serialize};
-use std::{collections::HashMap, fmt};
+use std::fmt;
 
 /// Enumerates the HTTP methods used by `tus::Client`.
 #[derive(Debug, Serialize, Deserialize)]
@@ -18,7 +15,11 @@ pub enum TusHttpMethod {
 
     /// Used to resume upload
     Patch,
+
+    /// Used to get server info
     Options,
+
+    /// used in termination extension
     Delete,
 }
 
@@ -32,44 +33,6 @@ impl TusHttpMethod {
             Self::Options => Method::OPTIONS,
             Self::Delete => Method::DELETE,
         }
-    }
-
-    pub fn required_headers(&self) -> Vec<String> {
-        let mut v = vec![tus::headers::TUS_RESUMABLE];
-
-        match self {
-            Self::Post => v.extend(vec![tus::headers::TUS_RESUMABLE]),
-            Self::Get => v.extend(vec![tus::headers::TUS_RESUMABLE]),
-            Self::Patch => v.extend(vec![
-                tus::headers::CONTENT_TYPE,
-                tus::headers::UPLOAD_OFFSET,
-                tus::headers::UPLOAD_LENGTH,
-            ]),
-            Self::Delete => v.extend(vec![tus::headers::TUS_RESUMABLE]),
-            _ => {}
-        };
-
-        v.into_iter().map(|s| s.to_string()).collect()
-    }
-
-    pub fn default_request_headers(&self) -> HashMap<String, String> {
-        let mut all = HashMap::<String, String>::new();
-        all.insert(
-            String::from(tus::headers::TUS_RESUMABLE),
-            String::from("1.0.0"),
-        );
-
-        all.extend(match self {
-            Self::Head => HashMap::new(),
-            // Self::Post => Method::POST,
-            // Self::Get => Method::GET,
-            // Self::Patch => Method::PATCH,
-            // Self::Options => Method::OPTIONS,
-            // Self::Delete => Method::DELETE,
-            _ => HashMap::new(),
-        });
-
-        all
     }
 }
 
--- src/tus/mod.rs
+++ src/tus/mod.rs
@@ -1,172 +1,13 @@
 pub mod errors;
 pub mod headers;
 pub mod http;
+pub mod ops;
 pub mod upload_meta;
 
-use crate::{error::TusError, tus};
+pub use ops::*;
 use reqwest::header::HeaderMap;
-use reqwest::Response;
 use serde;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-use std::path::PathBuf;
-use std::str::FromStr;
-
-use self::headers::UPLOAD_OFFSET;
-use self::http::TusHttpMethod;
-use self::upload_meta::UploadMeta;
-
-// use self::http::{Headers, HttpRequest};
-
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "lowercase")]
-pub enum TusOp {
-    // ************
-    // Core
-    // ************
-    /// Determines the offset at which the upload should be continued
-    ///
-    /// **Assumes URL for upload has already been created by the Creation extension**
-    ///
-    /// _Returns an error if not_
-    ///
-    /// Returns the `UploadMeta` with the `bytes_uploaded` updated with the
-    /// returned offset
-    GetOffset,
-
-    /// Resume upload
-    Upload,
-
-    // ************
-    // Extensions
-    // ************
-    /// The server supports creating files.
-    Creation,
-    //// The server supports setting expiration time on files and uploads.
-    Expiration,
-    /// The server supports verifying checksums of uploaded chunks.
-    Checksum,
-    /// The server supports deleting files.
-    Termination,
-    /// The server supports parallel uploads of a single file.
-    Concatenation,
-}
-
-impl From<TusOp> for TusHttpMethod {
-    fn from(extension: TusOp) -> Self {
-        extension.method()
-    }
-}
-
-impl TusOp {
-    pub fn method(&self) -> TusHttpMethod {
-        match self {
-            TusOp::GetOffset => TusHttpMethod::Head,
-
-            // all patch requests must contain
-            // "Content-Type": "application/offset+octet-stream"
-            TusOp::Upload => TusHttpMethod::Patch,
-            TusOp::Creation => TusHttpMethod::Post,
-            TusOp::Expiration => TusHttpMethod::Post,
-            TusOp::Checksum => TusHttpMethod::Post,
-            TusOp::Termination => TusHttpMethod::Post,
-            TusOp::Concatenation => TusHttpMethod::Post,
-        }
-    }
-
-    pub fn headers(&self, metadata: &UploadMeta) -> Result<HashMap<String, String>, TusError> {
-        let mut headers = tus::headers::default_headers();
-        let data = metadata.data64()?;
-        headers.insert(tus::headers::UPLOAD_METADATA.to_owned(), data);
-        if let Some(custom_headers) = &metadata.custom_headers {
-            headers.extend(custom_headers.clone());
-        }
-        match self {
-            TusOp::Creation => {
-                headers.insert(
-                    tus::headers::UPLOAD_LENGTH.to_owned(),
-                    format!("{}", metadata.status.size),
-                );
-            }
-            TusOp::Upload => {
-                headers.insert(
-                    tus::headers::CONTENT_TYPE.to_owned(),
-                    "application/offset+octet-stream".to_string(),
-                );
-                headers.insert(
-                    tus::headers::UPLOAD_LENGTH.to_owned(),
-                    format!("{}", metadata.status.size),
-                );
-                headers.insert(
-                    tus::headers::UPLOAD_OFFSET.to_owned(),
-                    format!("{}", metadata.status.bytes_uploaded),
-                );
-            }
-            _ => {}
-        }
-        Ok(headers)
-    }
-
-    pub fn handle_response(
-        &self,
-        response: Response,
-        metadata: &UploadMeta,
-    ) -> Result<UploadMeta, TusError> {
-        match self {
-            TusOp::Creation => {
-                let remote_dest = response
-                    .headers()
-                    .get(tus::headers::TUS_LOCATION)
-                    .ok_or(TusError::MissingHeader(
-                        tus::headers::TUS_LOCATION.to_owned(),
-                    ))?
-                    .to_str()
-                    .map_err(|_| TusError::MissingHeader(tus::headers::TUS_LOCATION.to_owned()))?;
-                let remote_dest = PathBuf::from_str(remote_dest)
-                    .map_err(|_| TusError::MissingHeader(tus::headers::TUS_LOCATION.to_owned()))?
-                    .into();
-                Ok(UploadMeta {
-                    remote_dest,
-                    ..metadata.clone()
-                })
-            }
-            TusOp::GetOffset => {
-                let offset = response
-                    .headers()
-                    .get(tus::headers::UPLOAD_OFFSET)
-                    .ok_or(TusError::RequestError)?
-                    .to_str()?;
-                let offset = str::parse::<usize>(offset)?;
-                Ok(metadata.with_bytes_uploaded(offset))
-            }
-            TusOp::Upload => {
-                // let headers: HashMap<String,String> = response
-                //     .headers()
-                //     .iter()
-                //     .map(|(k, v)| (format!("{k}"), format!("{}", v.to_string()))).collect();
-                let offset = response
-                    .headers()
-                    .get(UPLOAD_OFFSET)
-                    .map_or(None, |v| {
-                        str::parse::<usize>(&v.to_str().unwrap_or("")).ok()
-                    })
-                    .ok_or(TusError::MissingHeader(
-                        tus::headers::UPLOAD_OFFSET.to_owned(),
-                    ))?;
-                Ok(metadata.with_bytes_uploaded(offset))
-            }
-            _ => Ok(metadata.clone()),
-        }
-    }
-}
-
-impl FromStr for TusOp {
-    type Err = TusError;
-
-    fn from_str(value: &str) -> Result<Self, Self::Err> {
-        serde_json::from_str(&value).map_err(|_| TusError::SerdeError)
-    }
-}
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct UploadStatus {
@@ -269,4 +110,3 @@ impl From<HeaderMap> for TusServerInfo {
     }
 }
 
-// fn build_request<'a>(extension: TusOp) -> Result<HttpRequest<'a>, Error> {}
