f4054331763845ba33e3e97ec196521d44e146fb
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2024-04-09 13:04:46 -0400

    fix(tests): add client tests

---
+++ tests/client.rs
@@ -0,0 +1,37 @@
+use std::{io::Write, path::PathBuf, str::FromStr};
+
+use tus_rs::client::*;
+
+use tempfile::NamedTempFile;
+use url::{form_urlencoded::parse, Url};
+
+const TUS_ENDPOINT: &str = "http://127.0.0.1:8080/files/";
+
+fn create_temp_file() -> NamedTempFile {
+    let mut temp_file = NamedTempFile::new().unwrap();
+    let buffer: Vec<u8> = (0..(1024 * 1024)).map(|_| rand::random::<u8>()).collect();
+    for _ in 0..20 {
+        temp_file.write_all(&buffer[..]).unwrap();
+    }
+    temp_file
+}
+
+#[tokio::test]
+async fn should_get_server_info() {
+    let url = Url::parse(TUS_ENDPOINT).unwrap();
+    let client = Client::new();
+    let result = client.get_server_info(&url).await;
+    dbg!(&result);
+    assert!(result.is_ok());
+}
+
+#[tokio::test]
+async fn should_create_file() {
+    let temp_file = create_temp_file();
+    let path = temp_file.path().into();
+    let client = Client::new();
+    let host = Url::parse(TUS_ENDPOINT).unwrap();
+    let result = client.upload(&path, &host, None, None).await;
+    dbg!(&result);
+    assert!(result.is_ok());
+}
