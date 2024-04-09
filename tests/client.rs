628acd05ca8621a8edf75316174713cf0ca22ad4
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2024-04-09 13:44:58 -0400

    fix(tests): add creation specific test and more create temp files with different sizes

--- tests/client.rs
+++ tests/client.rs
@@ -8,9 +8,9 @@ use url::Url;
 
 const TUS_ENDPOINT: &str = "http://127.0.0.1:8080/files/";
 
-fn create_temp_file() -> NamedTempFile {
+fn create_temp_file(size: usize) -> NamedTempFile {
     let mut temp_file = NamedTempFile::new().unwrap();
-    let buffer: Vec<u8> = (0..(1024 * 1024)).map(|_| rand::random::<u8>()).collect();
+    let buffer: Vec<u8> = (0..size).map(|_| rand::random::<u8>()).collect();
     for _ in 0..20 {
         temp_file.write_all(&buffer[..]).unwrap();
     }
@@ -24,11 +24,19 @@ async fn should_get_server_info() {
     let result = client.get_server_info(&url).await;
     dbg!(&result);
     assert!(result.is_ok());
+    let result = result.unwrap();
+    assert!(result.extensions.len() > 0);
 }
 
 #[tokio::test]
 async fn should_create_file() {
-    todo!()
+    let temp_file = create_temp_file(128);
+    let path = temp_file.path().into();
+    let client = Client::new();
+    let host = Url::parse(TUS_ENDPOINT).unwrap();
+    let result = client.create(&path, &host, None, None).await;
+    dbg!(&result);
+    assert!(result.is_ok());
 }
 
 #[tokio::test]
@@ -38,7 +46,7 @@ async fn should_resume_file() {
 
 #[tokio::test]
 async fn should_create_and_upload_file() {
-    let temp_file = create_temp_file();
+    let temp_file = create_temp_file(1024 * 100);
     let path = temp_file.path().into();
     let client = Client::new();
     let host = Url::parse(TUS_ENDPOINT).unwrap();
